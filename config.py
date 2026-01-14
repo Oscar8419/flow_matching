6fb618c2498b681250d3c71487a5aa72caac9f50
fly <1964172017@qq.com>
2026-01-14 16:16:55 +0800

    modify config

--- config.py
+++ config.py
@@ -4,10 +4,15 @@ import torch
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 CONFIG = {
+    "times_log": 20,  # 每次训练报告 x 次损失日志
+    "times_save": 5,  # 每次训练保存 x 次模型参数
+    # Checkpoint paths
+    "fm_model_path": None,
+    "classifier_model_path": None,
     # Classifier training hyperparameters
     "classifier_model": "GRUformer",  # 可选: "GRU", "GRUformer"
-    "classifier_num_samples": 2e6,  # 分类训练总样本数量
-    "classifier_snr_range": (-10, 0),  # 分类器训练时的 SNR 范围 (dB)
+    "classifier_num_samples": 4e6,  # 分类训练总样本数量
+    "classifier_snr_range": (-10, 10),  # 分类器训练时的 SNR 范围 (dB)
 
     # FM training hyperparameters
     "learning_rate": 1e-4,
@@ -15,11 +20,6 @@ CONFIG = {
     "num_samples": 5e6,  # 总样本数量
     "signal_length": 1024,  # 每个信号的长度
     "num_workers": 8,  # DataLoader 的 num_workers
-    # "save_interval": 2e5,  # 每隔多少个 samples 保存一次模型, data are generated on the fly
-    "times_log": 20,  # 每次训练报告 x 次损失日志
-    "times_save": 5,  # 每次训练保存 x 次模型参数
-    "fm_model_path": "/root/code/flow_matching/checkpoints/01-11--16-21/model_final.pth",
-    "classifier_model_path": None,
 
     # model hyperparameters
     "patch_size": 16,
